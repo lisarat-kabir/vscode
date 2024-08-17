21a6e42900856237b60573a7990f58e207bc6dc6
lisarat-kabir <lisaratkabir0@gmail.com>
2024-08-17 16:53:03 +0600

    vscode
    Signed-off-by: lisarat-kabir <lisaratkabir0@gmail.com>

---
+++ multiFunction.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void myFunction(string name,int age){
+    cout<<name<<"kabir "<<", "<<"Age:"<<age<<"\n";
+}
+int main(){
+    myFunction("Lisarat ",22);
+     myFunction("Lutful ",25);
+      myFunction("Aryan ",26);
+       myFunction("Salah Uddin ",36);
+    return 0;
+}
