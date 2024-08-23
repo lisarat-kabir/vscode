43b92f065606ab4f4f9c32cc7891dacd0d5b6720
lisarat-kabir <lisaratkabir0@gmail.com>
2024-08-23 23:45:39 +0600

    Python
    Signed-off-by: lisarat-kabir <lisaratkabir0@gmail.com>

---
+++ function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void myFunction(string name){
+    cout<<name<<" Kabir.\n";
+
+}
+int main(){
+    myFunction("Lutful");
+    myFunction("Lisarat");
+    myFunction("Hasnan Zubayed");
+    myFunction("Aryan");
+    return 0;
+}
